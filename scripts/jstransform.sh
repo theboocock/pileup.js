692b814e5a387374414fd9ea1ba6164c79155d38
B. Arman Aksoy <arman@aksoy.org>
2015-06-16 13:40:38 -0400

    add test transformation to the jstransform script

--- scripts/jstransform.sh
+++ scripts/jstransform.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-jstransform --react --harmony --strip-types --non-strict-es6module lib/ src/
+export JSTRANSFORM_OPTS="--react --harmony --strip-types --non-strict-es6module"
+jstransform $JSTRANSFORM_OPTS lib/ src/
+jstransform $JSTRANSFORM_OPTS lib-test/ test/
